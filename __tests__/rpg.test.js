2cad01ea26c2f9e19575d0d184119fe4b534bfb1
Taylor Phillips <taylorphillips133@gmail.com>
2020-04-16 10:26:18 -0700

    show that our test now is passing which confirms our Character Constructor

--- __tests__/rpg.test.js
+++ __tests__/rpg.test.js
@@ -4,6 +4,6 @@ describe('Character', () => {
   
   test('should test that our constructor properly assigns the proper value', () => {
     let test1 = new Character(5, 7, 10);
-    expect(test1.might).toEqual(8);
+    expect(test1.might).toEqual(5);
   });
 });
