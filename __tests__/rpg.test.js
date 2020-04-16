5ff82301d6bc5b6008c6f9fc74bffeaa20077e79
Taylor Phillips <taylorphillips133@gmail.com>
2020-04-16 10:40:27 -0700

    show that our test for character specialty failed as we thought it would

--- __tests__/rpg.test.js
+++ __tests__/rpg.test.js
@@ -2,8 +2,13 @@ import { Character } from './../src/character.js';
 
 describe('Character', () => {
   
-  test('should test that our constructor properly assigns the proper value', () => {
-    let test1 = new Character(5, 7, 10);
-    expect(test1.might).toEqual(5);
+  // test('should test that our constructor properly assigns the proper value', () => {
+  //   let test1 = new Character(5, 7, 10);
+  //   expect(test1.might).toEqual(5);
+  // });
+
+  test('Should test that specialty is a valid selection', () => {
+    let test2 = new Character("barbarian");
+    expect(test2.might).toEqual(7);
   });
 });
--- src/character.js
+++ src/character.js
@@ -1,8 +1,20 @@
 export class Character {
-  constructor(might, wisdom, health) {
-    this.might = might;
-    this.wisdom = wisdom;
-    this.health = health;
+  constructor(specialty) {
+    if (specialty == "barbarian") {
+      this.might = 10;
+      this.wisdom = 5;
+      this.health = 10;
+    }
+    if (specialty == "wizard") {
+      this.might = 5;
+      this.wisdom = 10;
+      this.health = 10;
+    }
+    if (specialty == "paladin") {
+      this.might = 8;
+      this.wisdom = 8;
+      this.health = 10;
+    }
   }
 }
 
